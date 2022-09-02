341e1c70a60f7abc272c48a1eb110283a0f7bf51
Conrado <conrado.spereira@gmail.com>
2022-09-02 00:22:05 -0300

    Creating TaskRepository extend Jpa.

---
+++ src/main/java/com/project/todo/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.project.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.todo.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+}
