313c087b29c004fd9fcf754285651391a24c3264
Conrado <conrado.spereira@gmail.com>
2022-09-02 13:00:40 -0300

    Creating method list all tasks.

--- src/main/java/com/project/todo/service/TaskService.java
+++ src/main/java/com/project/todo/service/TaskService.java
@@ -1,5 +1,7 @@
 package com.project.todo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,9 @@ public class TaskService {
 
     public Task createTask (Task task){
         return taskRepository.save(task);
+    }
+
+    public List<Task> listAllTasks() {
+        return taskRepository.findAll();
     }
 }
