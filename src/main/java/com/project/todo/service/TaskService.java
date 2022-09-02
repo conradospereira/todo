71a275f1965cd395368ccc28aff59fc8a8728d72
Conrado <conrado.spereira@gmail.com>
2022-09-02 00:25:08 -0300

    Create TaskService.

---
+++ src/main/java/com/project/todo/service/TaskService.java
@@ -0,0 +1,21 @@
+package com.project.todo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.todo.model.Task;
+import com.project.todo.repository.TaskRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask (Task task){
+        return taskRepository.save(task);
+    }
+}
