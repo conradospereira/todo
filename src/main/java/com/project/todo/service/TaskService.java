86884377dabf64e4c8dd0e669aaf07d29aa2f523
Conrado <conrado.spereira@gmail.com>
2022-09-02 13:12:50 -0300

    Creating methods controller and service get by id.

--- src/main/java/com/project/todo/service/TaskService.java
+++ src/main/java/com/project/todo/service/TaskService.java
@@ -3,6 +3,7 @@ package com.project.todo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.project.todo.model.Task;
@@ -23,5 +24,11 @@ public class TaskService {
 
     public List<Task> listAllTasks() {
         return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> findTaskById(Long id){
+        return taskRepository.findById(id)
+                .map(task -> ResponseEntity.ok().body(task))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
