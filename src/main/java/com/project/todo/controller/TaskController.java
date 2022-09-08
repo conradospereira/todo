642fb534ed2f1312712cdba7b72e6a8d0906b5db
Conrado <conrado.spereira@gmail.com>
2022-09-08 00:34:14 -0300

    Creating configurations springfox on endpoints.

--- src/main/java/com/project/todo/controller/TaskController.java
+++ src/main/java/com/project/todo/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.todo.model.Task;
 import com.project.todo.service.TaskService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,12 +31,24 @@ public class TaskController {
 
     TaskService taskService;
 
+    @ApiOperation(value = "Creating a new task.")
+    @ApiResponses(value ={
+        @ApiResponse(code = 201, message = "Task created successfully"),
+        @ApiResponse(code = 500, message = "Error creating task.")
+    })
+
     @PostMapping("/tasks")
     @ResponseStatus(HttpStatus.CREATED)
     public Task createTask (@RequestBody Task task){
         log.info("Creating a new task with they information");
         return taskService.createTask(task);
     }
+
+    @ApiOperation(value = "Listing all classes.")
+    @ApiResponses(value ={
+        @ApiResponse(code = 200, message = "Tasks listed successfully"),
+        @ApiResponse(code = 500, message = "Error listing tasks.")
+    })
 
     @GetMapping("/tasks")
     @ResponseStatus(HttpStatus.OK)
@@ -41,12 +57,24 @@ public class TaskController {
         return taskService.listAllTasks();
     }
 
+    @ApiOperation(value = "Search a task by id.")
+    @ApiResponses(value ={
+        @ApiResponse(code = 200, message = "Tasks found successfully"),
+        @ApiResponse(code = 404, message = "No tasks found.")
+    })
+
     @GetMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> getTaskById(@PathVariable (value = "id") Long id){
         log.info("Searching task by your id [{}]");
         return taskService.findTaskById(id);
     }
+
+    @ApiOperation(value = "Updating a task.")
+    @ApiResponses(value ={
+        @ApiResponse(code = 200, message = "Tasks updated successfully"),
+        @ApiResponse(code = 404, message = "Could not update task.")
+    })
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
@@ -56,4 +84,10 @@ public class TaskController {
         return taskService.updateTaskById(task, id);
     }
     
+    @DeleteMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deleteTaskById(@PathVariable (value = "id") Long id) {
+        log.info("Deleting task with id [{}]", id);
+        return taskService.deleteById(id);
+    }
 }
